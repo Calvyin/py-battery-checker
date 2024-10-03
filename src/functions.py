932ffcd2654d48e1bdad4e11dac995ba6032ddce
calvyin <cynicmuffin@gmail.com>
2024-10-03 22:09:57 +0530

    commit
    Signed-off-by: calvyin <cynicmuffin@gmail.com>

--- src/functions.py
+++ src/functions.py
@@ -1,6 +1,7 @@
 import psutil as ps
 import plyer as pl
 import time
+import tkinter as tk
 
 
 def print_bat_life():
@@ -17,9 +18,20 @@ def bat_check():
     while charged is False:
         time.sleep(120.0 - ((time.monotonic() - starttime) % 120.0))
         if bat.percent == 60:
-            pl.notification.notify(title='Battery Status', message='Your laptop battery has reached 60%\nUnplug your '
+            root = tk.Tk()
+            root.overrideredirect(True)
+            root.attributes("-topmost", True)
+            label = tk.Label(root, text = "Your battery has reached 60%", font = ("Arial", 60))
+            label.pack(expand = True)
+            root.geometry("300x100+1000+100")
+            root.after(10000, root.destroy)
+            root.mainloop()
+
+            '''pl.notification.notify(title='Battery Status', message='Your laptop battery has reached 60%\nUnplug your '
                                                                    'charger', app_name='Battery checker', app_icon='',
-                                   timeout=10, ticker='', toast=False, hints={})
+                                   timeout=10, ticker='', toast=False, hints={})'''
             charged = True
         else:
             pass
+    if charged is True:
+        exit()
